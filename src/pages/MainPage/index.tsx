1c9dc86fb86f0a422e9e0f94b368565a7cac6d88
Kang Minji <mminjg@gmail.com>
2022-06-27 14:20:49 +0900

    🔧 craco 절대 경로 적용

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainTemplate from '../../templates/MainTemplate';
+import MainTemplate from '@templates/MainTemplate';
 
 const MainPage: React.FC = () => {
   return <MainTemplate />;
