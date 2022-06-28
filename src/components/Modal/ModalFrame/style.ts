51a7f2211419dce7461f72a47c9e73193062384f
Kang Minji <mminjg@gmail.com>
2022-06-28 17:23:34 +0900

    💄 공통 모달 frame 구현
    모달의 크기와, child, close 메서드를 가지는 공통 모달 frame 컴포넌트 구현

---
+++ src/components/Modal/ModalFrame/style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { ColorCode } from '@utils/constants';
+
+interface ContentContainerProps {
+  w: number;
+  h: number;
+}
+
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(0.25rem);
+`;
+
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentContainer = styled.div<ContentContainerProps>`
+  position: relative;
+  width: ${(props) => `${props.w}rem`};
+  height: ${(props) => `${props.h}rem`};
+  background-color: ${ColorCode.WHITE};
+  border-radius: 1.8rem;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.25);
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 1.7rem;
+  right: 1.7rem;
+`;
