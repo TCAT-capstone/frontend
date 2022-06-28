bae40000e214355726ad8048ba7ee4be2453fddf
Kang Minji <mminjg@gmail.com>
2022-06-28 14:14:29 +0900

    🚚 path 대소문자 수정

--- src/components/common/ProfileIcon/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Container } from './style';
-
-const ProfileIcon: React.FC = () => {
-  return <Container />;
-};
-
-export default ProfileIcon;
--- src/components/common/ProfileIcon/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { ColorCode } from '@utils/constants';
-
-export const Container = styled.div`
-  width: 2.2rem;
-  height: 2.2rem;
-  border-radius: 50%;
-  background-color: ${ColorCode.GRAY1};
-`;
