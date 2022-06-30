3e3963e406d6f1be98a8cc8430dc368b1fe60b50
Kang Minji <mminjg@gmail.com>
2022-06-30 13:12:55 +0900

    💄 프로필 아이콘 컴포넌트 구현

--- src/components/Common/ProfileIcon/index.tsx
+++ src/components/Common/ProfileIcon/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import { Container } from './style';
+import userImg from '@images/sample-user-img.jpg';
 
-const ProfileIcon: React.FC = () => {
-  return <Container />;
+import { Image } from './style';
+
+interface Props {
+  size: number;
+}
+
+const ProfileIcon: React.FC<Props> = ({ size }) => {
+  return <Image src={userImg} alt="프로필 사진" size={size} />;
 };
 
 export default ProfileIcon;
