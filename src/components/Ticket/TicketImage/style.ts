7d4c337479f19f3ff261c28de95a76a9bd35309d
bokyungkim <ohholyday@naver.com>
2022-07-02 05:03:34 +0900

    💄 티켓 목록 구현

---
+++ src/components/Ticket/TicketImage/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { ColorCode } from '@utils/constants';
+
+export const Image = styled.img`
+  width: 13.5rem;
+  border-radius: 0.5rem;
+  background-color: ${ColorCode.GRAY1};
+`;
---
+++ src/components/Ticket/index.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+
+import { Container } from './style';
+import TicketFrame from './TicketFrame';
+
+const Ticket: React.FC = () => {
+  return (
+    <Container>
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+      <TicketFrame />
+    </Container>
+  );
+};
+
+export default Ticket;
