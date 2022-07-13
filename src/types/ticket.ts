ed45549573e97a279fbf8d3d5bb83cedda316919
Kang Minji <mminjg@gmail.com>
2022-07-13 22:46:36 +0900

    🏷️ ticket req, res type 정의

--- src/types/ticket.ts
+++ src/types/ticket.ts
@@ -4,6 +4,7 @@ type CategoryType = 'MUSICAL' | 'CONCERT' | 'MOVIE' | 'SPORT' | 'ETC';
 
 export interface TicketSimpleType {
   ticketId: number;
+  homeId: number;
   memberName: string;
   ticketImg: string;
   title: string;
@@ -12,3 +13,37 @@ export interface TicketSimpleType {
 }
 
 export type TicketListResType = TicketSimpleType[];
+
+export interface TicketType {
+  ticketId: number;
+  ticketbookId: number;
+  memberId: number;
+  memberImg: string;
+  memberName: string;
+  memberBio: string;
+  ticketImg: string;
+  ticketValidation: TicketValidationType;
+  ticketTitle: string;
+  ticketDate: Date;
+  ticketSeat: string;
+  ticketLocation: string;
+  title: string;
+  content: string;
+  date: Date;
+  category: CategoryType;
+}
+
+export interface TicketReqType {
+  memberId: number;
+  ticketbookId: number;
+  ticketImg: string;
+  ticketValidation: TicketValidationType;
+  ticketTitle: string;
+  ticketDate: Date;
+  ticketSeat: string;
+  ticketLocation: string;
+  casting: string;
+  title: string;
+  content: string;
+  category: CategoryType;
+}
