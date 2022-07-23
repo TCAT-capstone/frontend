e16d715845406a44e2a3e8b6f84e26eba92c8a0d
Kang Minji <mminjg@gmail.com>
2022-07-23 19:36:50 +0900

    ✨ api header에 authorization 추가, member api 작성
    api를 요청할 때 로컬 스토리지에서 토큰을 가져와 헤더에 추가하여 전송합니다. member profile 정보를 가져오는 api를 작성하였습니다.

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -2,12 +2,21 @@ import fetchApi from '@utils/fetch';
 
 import { ProfileType } from '@src/types/member';
 
+export const getMyProfile = async (): Promise<ProfileType | false> => {
+  try {
+    const res = await fetchApi.get(`/api/members/my-profile`);
+    if (res.status !== 200) throw new Error('error');
+    return await res.json();
+  } catch (err) {
+    return false;
+  }
+};
+
 export const getMemberProfile = async (memberId: number): Promise<ProfileType | false> => {
   try {
     const res = await fetchApi.get(`/api/members/${memberId}/profile`);
     if (res.status !== 200) throw new Error('error');
-    const data = await res.json();
-    return data;
+    return await res.json();
   } catch (err) {
     return false;
   }
