54832dde1d557a8b7efac61d16e8f356bf3ac4fd
bokyungkim <ohholyday@naver.com>
2022-09-16 16:08:26 +0900

    ⚡️ 티켓 불러오기 기능 추가

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -4,36 +4,75 @@ import SearchTemplate from '@templates/SearchTemplate';
 
 import { getSearchTickets } from '@apis/ticket';
 import { TicketListType } from '@src/types/ticket';
+import useInfiniteScroll from '@src/hooks/useInfiniteScroll';
+
+type CursorType = {
+  cursorId: number | null;
+};
+
+const initialCursor = {
+  cursorId: null,
+};
 
 const SearchPage: React.FC = () => {
   const [tickets, setTickets] = useState<TicketListType>([]);
-  const [option, setOption] = useState('통합검색');
+  const [cursor, setCursor] = useState<CursorType>(initialCursor);
+  const [keyword, setKeyword] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
-  const [target, setTarget] = useState();
+  const [hasNotTicket, setHasNoTicket] = useState(false);
+  const { apiTrigger, setTarget } = useInfiniteScroll();
 
-  const getTickets = async () => {
-    const data = await getSearchTickets();
-    if (data) {
-      setTickets(data.tickets);
+  const [condition, setCondition] = useState({
+    title: '',
+    date: '',
+    place: '',
+    seat: '',
+  });
+
+  const getTickets = async (
+    cursorId: number | null,
+    keyword: string | null,
+    ticketTitle: string | null,
+    ticketDate: string | null,
+    ticketSeat: string | null,
+    ticketLocation: string | null,
+  ) => {
+    if (!hasNotTicket) {
+      setIsLoaded(true);
+      const data = await getSearchTickets(cursorId, keyword, ticketTitle, ticketDate, ticketLocation, ticketSeat);
+      if (data) {
+        if (data.hasNotTicket) {
+          setHasNoTicket(true);
+        } else {
+          setTickets((prev) => [...prev, ...data.tickets]);
+          setCursor({
+            cursorId: data.tickets[data.tickets.length - 1].ticketId,
+          });
+        }
+      }
+      setIsLoaded(false);
     }
   };
 
-  const handleOptionChange = (e: React.ChangeEvent<HTMLButtonElement>) => {
-    setOption((e.target as HTMLInputElement).value);
+  const search = ({ title, date, place, seat }: { title: string; date: string; place: string; seat: string }) => {
+    if (condition.title !== title || condition.date !== date || condition.place !== place || condition.seat !== seat) {
+      setCondition({
+        title,
+        date,
+        place,
+        seat,
+      });
+    }
   };
 
   useEffect(() => {
-    getTickets();
-  }, []);
+    if (apiTrigger > 0) {
+      getTickets(cursor.cursorId, keyword, condition.title, condition.date, condition.place, condition.seat);
+    }
+  }, [apiTrigger]);
 
   return (
-    <SearchTemplate
-      tickets={tickets}
-      option={option}
-      handleOptionChange={handleOptionChange}
-      isLoaded={isLoaded}
-      setTarget={setTarget}
-    />
+    <SearchTemplate tickets={tickets} isLoaded={isLoaded} setTarget={setTarget} condition={condition} search={search} />
   );
 };
 
