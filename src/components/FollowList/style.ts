c6bd62dbd2c352552c298b95fa9f20e9796de15e
bokyungkim <ohholyday@naver.com>
2022-09-20 23:59:43 +0900

    💄  FollowList 추가

---
+++ src/components/FollowList/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  width: 36rem;
+  height: 35.37rem;
+`;
