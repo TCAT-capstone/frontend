2bc2d9fb954570ef9cba34f045d367f1599dfe5c
Kang Minji <mminjg@gmail.com>
2022-10-03 23:45:41 +0900

    ♻️ input 디바운싱 적용
    input에 변화가 일어날 때 디바운싱을 적용하여 api통신을 하도록 수정하였습니다. 또한 검색에서 엔터와 클릭을 통한 검색을 제거하였습니다.

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,27 +1,20 @@
-import React, { KeyboardEvent } from 'react';
+import React from 'react';
 
 import searchImg from '@images/search.svg';
-import { Container, SearchContent, SearchButton } from './style';
+import { Container, SearchContent, SearchIcon } from './style';
 
 interface Props {
   keyword: string;
-  search: () => void;
-  onSubmitSearch: (e: KeyboardEvent<HTMLInputElement>) => void;
   handleKeywordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchBox: React.FC<Props> = ({ keyword, search, onSubmitSearch, handleKeywordChange }) => {
+const SearchBox: React.FC<Props> = ({ keyword, handleKeywordChange }) => {
   return (
     <Container>
-      <SearchContent
-        onChange={handleKeywordChange}
-        value={keyword}
-        placeholder="검색어를 입력하세요"
-        onKeyPress={onSubmitSearch}
-      />
-      <SearchButton onClick={search}>
+      <SearchContent onChange={handleKeywordChange} value={keyword} placeholder="검색어를 입력하세요" />
+      <SearchIcon>
         <img src={searchImg} alt="검색" width="18rem" />
-      </SearchButton>
+      </SearchIcon>
     </Container>
   );
 };
