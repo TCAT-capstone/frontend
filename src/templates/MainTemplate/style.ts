6a68f75082db34a5baded6c4d15f8603c547726e
bokyungkim <ohholyday@naver.com>
2022-11-18 14:22:38 +0900

    ⚡️  메인 페이지 하단 notion 이동 버튼 추가

--- src/templates/MainTemplate/style.ts
+++ src/templates/MainTemplate/style.ts
@@ -6,3 +6,12 @@ export const TabWrapper = styled.div`
   margin-top: 1.5rem;
   margin-bottom: 2.5rem;
 `;
+
+export const Help = styled.a`
+  position: fixed;
+  bottom: 1rem;
+  left: 1rem;
+  img {
+    width: 1.7rem;
+  }
+`;
